7bdebbbf88c614f001ba88c5329b67f1f4a29c5f
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-03 11:31:23 +0530

    JWTUserrAuthServiceController (API with read write action) added

--- JWTbearerAuthenticationCoreWebAPI/Startup.cs
+++ JWTbearerAuthenticationCoreWebAPI/Startup.cs
@@ -1,4 +1,5 @@
 using JWTbearerAuthenticationCoreWebAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -7,10 +8,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace JWTbearerAuthenticationCoreWebAPI
@@ -32,6 +35,33 @@ namespace JWTbearerAuthenticationCoreWebAPI
             services.AddDbContextPool<AppDBContext>(
               options => options.UseSqlServer(Configuration.GetConnectionString("MyDBConnection"))
               );
+
+            //using Microsoft.Extensions.Configuration;
+
+            //Just for a reference having security key in appsetting file
+            //ValidIssuer = Configuration["Jwt:Issuer"],
+            //        ValidAudience = Configuration["Jwt:Issuer"],
+            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+            //Refernece end
+
+            var key = "This is my jwt authentication demo";
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+
+                };
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWTbearerAuthenticationCoreWebAPI", Version = "v1" });
