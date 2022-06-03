40d00ef8069b98d429367d2de81ad3d583bf285c
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-03 11:13:06 +0530

    IJwtAuthenticationManager interface added and implemented in JwtAuthenticationManager

---
+++ JWTbearerAuthenticationCoreWebAPI/Models/IJwtAuthenticationManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTbearerAuthenticationCoreWebAPI.Models
+{
+   public  interface IJwtAuthenticationManager
+    {
+        string Authenticate(string email, string username, string password);
+
+    }
+}
---
+++ JWTbearerAuthenticationCoreWebAPI/Models/JwtAuthenticationManager.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTbearerAuthenticationCoreWebAPI.Models
+{
+    public class JwtAuthenticationManager : IJwtAuthenticationManager
+    {
+        private readonly string Key;
+
+        public JwtAuthenticationManager(string Key)
+        {
+            this.Key = Key;
+        }
+
+
+
+        public string Authenticate(string email, string username, string password)
+        {
+
+            var tokenHandler = new JwtSecurityTokenHandler();// install System.IdentityModel.Tokens.Jwt
+            var tokenKey = Encoding.ASCII.GetBytes(Key);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Email, email),
+                    new Claim(ClaimTypes.Name,username)
+                }
+                ),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(tokenKey),
+                    SecurityAlgorithms.HmacSha256Signature)
+
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+
+        }
+    }
+}
