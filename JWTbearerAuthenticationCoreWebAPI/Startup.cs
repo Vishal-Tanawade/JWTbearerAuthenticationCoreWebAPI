dce5714ad751beb6f37ad060ec38e75f64d40325
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-03 10:58:43 +0530

    Connection String added in JSON - also specified in startup file

--- JWTbearerAuthenticationCoreWebAPI/Startup.cs
+++ JWTbearerAuthenticationCoreWebAPI/Startup.cs
@@ -1,6 +1,8 @@
+using JWTbearerAuthenticationCoreWebAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,9 @@ namespace JWTbearerAuthenticationCoreWebAPI
         {
 
             services.AddControllers();
+            services.AddDbContextPool<AppDBContext>(
+              options => options.UseSqlServer(Configuration.GetConnectionString("MyDBConnection"))
+              );
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWTbearerAuthenticationCoreWebAPI", Version = "v1" });
